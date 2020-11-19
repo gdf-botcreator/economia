52fb9fcdecb10b2bf000f88a548e3fb0722690ed
gdf-botcreator <74744576+gdf-botcreator@users.noreply.github.com>
2020-11-19 20:33:42 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  work: require("./src/work.js"),
+  balance: require("./src/balance.js"),
+  pay: require("./src/pay.js"),
+  daily: require("./src/daily.js"),
+  beg: require("./src/beg.js"),
+  leaderboard: require("./src/leaderboard.js")
+};
